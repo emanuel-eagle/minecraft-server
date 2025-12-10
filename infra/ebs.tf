1873fa402f8f58040a4d355f5865a964567d00c5
Emanuel Eagle <eeagle@Emanuels-MacBook-Pro.local>
2025-12-09 23:29:04 -0500

    ripping all the cost centers out for now

--- infra/ebs.tf
+++ infra/ebs.tf
@@ -1,30 +1,30 @@
 
-resource "aws_ebs_volume" "minecraft_data" {
-  availability_zone = var.availability_zone
-  size              = var.ebs_volume_size
-  type              = var.ebs_volume_type
+# resource "aws_ebs_volume" "minecraft_data" {
+#   availability_zone = var.availability_zone
+#   size              = var.ebs_volume_size
+#   type              = var.ebs_volume_type
   
-  tags = {
-    Name = var.ebs_volume_name
-    Project = var.project_name
-  }
-}
+#   tags = {
+#     Name = var.ebs_volume_name
+#     Project = var.project_name
+#   }
+# }
 
-# Attach the volume to the instance
-resource "aws_volume_attachment" "minecraft_data" {
-  device_name = "/dev/sdf"
-  volume_id   = aws_ebs_volume.minecraft_data.id
-  instance_id = aws_instance.minecraft-server.id
-  force_detach = true       
-  skip_destroy = false      
-  stop_instance_before_detaching = false
-}
+# # Attach the volume to the instance
+# resource "aws_volume_attachment" "minecraft_data" {
+#   device_name = "/dev/sdf"
+#   volume_id   = aws_ebs_volume.minecraft_data.id
+#   instance_id = aws_instance.minecraft-server.id
+#   force_detach = true       
+#   skip_destroy = false      
+#   stop_instance_before_detaching = false
+# }
 
-resource "aws_ebs_snapshot" "minecraft_data_snapshot" {
-  volume_id = aws_ebs_volume.minecraft_data.id
-  region = var.primary_region
-  tags = {
-    Name = "${var.ebs_volume_name}-snapshot"
-    Project = var.project_name
-  }
-}
+# resource "aws_ebs_snapshot" "minecraft_data_snapshot" {
+#   volume_id = aws_ebs_volume.minecraft_data.id
+#   region = var.primary_region
+#   tags = {
+#     Name = "${var.ebs_volume_name}-snapshot"
+#     Project = var.project_name
+#   }
+# }
--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,38 +1,38 @@
-resource "aws_instance" "minecraft-server" {
-  ami           = data.aws_ami.amazon_linux_2023.id
-  iam_instance_profile = aws_iam_instance_profile.minecraft_profile.name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.minecraft-server-sg.id]
-  subnet_id = aws_subnet.minecraft-subnet.id
-  associate_public_ip_address = true
-  user_data_replace_on_change = true  
+# resource "aws_instance" "minecraft-server" {
+#   ami           = data.aws_ami.amazon_linux_2023.id
+#   iam_instance_profile = aws_iam_instance_profile.minecraft_profile.name
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [aws_security_group.minecraft-server-sg.id]
+#   subnet_id = aws_subnet.minecraft-subnet.id
+#   associate_public_ip_address = true
+#   user_data_replace_on_change = true  
 
-  # Ensure network infra (IGW + public route association) exists before instance/EIP association
-  depends_on = [
-    aws_internet_gateway.igw,
-    aws_route_table_association.public_assoc
-  ]
-  availability_zone = var.availability_zone
-  user_data = templatefile(var.user_data_script, {
-    difficulty = var.minecraft_difficulty
-    max_players = var.minecraft_max_players
-    view_distance = var.minecraft_view_distance
-    gamemode = var.minecraft_gamemode 
-    port = var.minecraft_port_number
-    simulation_distance = var.minecraft_simulation_distance
-    whitelist_json = jsonencode(var.minecraft_whitelist)
-    minecraft_version   = var.minecraft_version  
-  })
-  instance_market_options {
-    market_type = var.market_type
-    spot_options {
-        max_price = var.max_price # minimum max price is 0.0032 for t3.micro in us-east-2
-        spot_instance_type = var.spot_instance_type
-        instance_interruption_behavior = var.instance_interruption_behavior
-        }
-    }
-  tags = {
-    Project = var.project_name
-    Name = var.server_name
-  }
-}
+#   # Ensure network infra (IGW + public route association) exists before instance/EIP association
+#   depends_on = [
+#     aws_internet_gateway.igw,
+#     aws_route_table_association.public_assoc
+#   ]
+#   availability_zone = var.availability_zone
+#   user_data = templatefile(var.user_data_script, {
+#     difficulty = var.minecraft_difficulty
+#     max_players = var.minecraft_max_players
+#     view_distance = var.minecraft_view_distance
+#     gamemode = var.minecraft_gamemode 
+#     port = var.minecraft_port_number
+#     simulation_distance = var.minecraft_simulation_distance
+#     whitelist_json = jsonencode(var.minecraft_whitelist)
+#     minecraft_version   = var.minecraft_version  
+#   })
+#   instance_market_options {
+#     market_type = var.market_type
+#     spot_options {
+#         max_price = var.max_price # minimum max price is 0.0032 for t3.micro in us-east-2
+#         spot_instance_type = var.spot_instance_type
+#         instance_interruption_behavior = var.instance_interruption_behavior
+#         }
+#     }
+#   tags = {
+#     Project = var.project_name
+#     Name = var.server_name
+#   }
+# }
