5e40aec82bab73ade74fa871f8393b0bb58b339b
Emanuel Eagle <eeagle@Emanuels-MacBook-Pro.local>
2025-12-09 20:47:49 -0500

    deleting ebs volume and elastic ip

--- infra/ebs.tf
+++ infra/ebs.tf
@@ -1,28 +1,28 @@
 
-resource "aws_ebs_volume" "minecraft_data" {
-  availability_zone = var.availability_zone
-  size              = var.ebs_volume_size
-  type              = var.ebs_volume_type
+# resource "aws_ebs_volume" "minecraft_data" {
+#   availability_zone = var.availability_zone
+#   size              = var.ebs_volume_size
+#   type              = var.ebs_volume_type
   
-  tags = {
-    Name = var.ebs_volume_name
-    Project = var.project_name
-  }
-}
+#   tags = {
+#     Name = var.ebs_volume_name
+#     Project = var.project_name
+#   }
+# }
 
-# Attach the volume to the instance
-resource "aws_volume_attachment" "minecraft_data" {
-  device_name = "/dev/sdf"
-  volume_id   = aws_ebs_volume.minecraft_data.id
-  instance_id = aws_instance.minecraft-server.id
-  skip_destroy = true
-}
+# # Attach the volume to the instance
+# resource "aws_volume_attachment" "minecraft_data" {
+#   device_name = "/dev/sdf"
+#   volume_id   = aws_ebs_volume.minecraft_data.id
+#   instance_id = aws_instance.minecraft-server.id
+#   skip_destroy = true
+# }
 
-resource "aws_ebs_snapshot" "minecraft_data_snapshot" {
-  volume_id = aws_ebs_volume.minecraft_data.id
-  region = var.primary_region
-  tags = {
-    Name = "${var.ebs_volume_name}-snapshot"
-    Project = var.project_name
-  }
-}
+# resource "aws_ebs_snapshot" "minecraft_data_snapshot" {
+#   volume_id = aws_ebs_volume.minecraft_data.id
+#   region = var.primary_region
+#   tags = {
+#     Name = "${var.ebs_volume_name}-snapshot"
+#     Project = var.project_name
+#   }
+# }
